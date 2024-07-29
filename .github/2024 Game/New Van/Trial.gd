517f4c13b60051db5c803898bfd6c5c95de9a5b7
BiggieD036 <169103930+BiggieD036@users.noreply.github.com>
2024-07-30 10:26:19 +1200

    Van changes and Road placement
    I've started to make my road and made some changes to the van.

--- .github/2024 Game/New Van/Trial.gd
+++ .github/2024 Game/New Van/Trial.gd
@@ -1,22 +1,23 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.5
-const ENGINE_POWER = 100
-
-
+const ENGINE_POWER = 50
 
 @onready var camera_pivot = $CameraPivot
 @onready var camera_3d = $CameraPivot/Camera3D
 
-
+var look_at
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	look_at = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("vi_down", "ui_up") * ENGINE_POWER
-	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
+	engine_force = (Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")) * ENGINE_POWER
+	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 5.0)
+	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 2.5)
+	look_at = look_at.lerp(global_position + linear_velocity, delta * 2.5)
+	camera_3d.look_at(look_at)
