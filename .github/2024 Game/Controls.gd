e50e446980abb69c034d44e7c06db4b2f1a03360
BiggieD036 <169103930+BiggieD036@users.noreply.github.com>
2024-09-23 18:21:23 +1200

    Test Finish
    I think my game may have been corrpued pushing this to see if it will fully runn

--- .github/2024 Game/Controls.gd
+++
@@ -1,6 +0,0 @@
-extends VBoxContainer
-
-const MAIN_MENU = "res://Main Menu.tscn"
-
-func _on_return_pressed():
-	get_tree().change_scene_to_file(MAIN_MENU)
--- .github/2024 Game/Main Menu.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var pivot = $Pivot
-
-var rotation_speed = 8
-
-func _process(delta):
-	pivot.rotation_degrees.y += delta * rotation_speed
